5563c400d41f5980985373d5c63299466404a722
Valentín Lorente <valenlj7@gmail.com>
2023-09-17 12:07:45 +0200

    Add new results endpoints

--- app/api/results.py
+++ app/api/results.py
@@ -3,11 +3,26 @@ from fastapi import APIRouter
 from app.database import SessionLocal
 from app.selectors.results import search_results
 
-PREFIX = '/results'
 router = APIRouter()
 session = SessionLocal()
 
-@router.get(PREFIX)
-def get_results():
-    results = search_results(session)
-    return results
+@router.get('')
+async def get_results():
+    result = search_results(session)
+    return result
+
+@router.get('/{team}')
+async def get_team(team: str):
+    filters = {
+        'team': team,
+    }
+    result = search_results(session, filters=filters)
+    return result
+
+@router.get('/{season}')
+async def get_season(season: str):
+    filters = {
+        'season': season,
+    }
+    result = search_results(session, filters=filters)
+    return result
